6c024f22c7435832cbc1eadc2eb551400647db0a
allecramarcella <allecramarcella.dev@gmail.com>
2021-02-12 15:14:16 +0100

    lessons until 18

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,8 +1,11 @@
-import { User } from './schemas/User';
 import { config, createSchema } from '@keystone-next/keystone/schema';
-import { createAuth } from '@keystone-next/auth'
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
+import { createAuth } from '@keystone-next/auth';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -35,11 +38,18 @@ export default withAuth(config ({
     db: {
         adapter: 'mongoose',
         url: databaseURL,
-        // TODO: Add data seeding here
+        async onConnect(keystone) {
+            console.log('Connected to the database!!');
+            if(process.argv.includes('--seed-data')){
+                await insertSeedData(keystone);
+            }            
+        },
     },
     lists: createSchema({
         //Schema items go in here
-        User
+        User,
+        Product,
+        ProductImage,
     }),
     ui: {
         // Show the UI only for people who pass this test
